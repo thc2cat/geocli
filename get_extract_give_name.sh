52a8b91fd638a3bd694b15b07dfc812132a84aef
Thierry Caillet <thierry.caillet@uvsq.fr>
2022-09-06 14:03:59 +0200

    update licence_key

--- get_extract_give_name.sh
+++ get_extract_give_name.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 
-wget -q "https://download.maxmind.com/app/geoip_download?edition_id=GeoLite2-Country&license_key=nRVkwS5Q0bWQTyAq&suffix=tar.gz" -O  GeoLite2-Country.tar.gz
+. ./license_key
+
+#wget -q "https://download.maxmind.com/app/geoip_download?edition_id=GeoLite2-Country&license_key=nRVkwS5Q0bWQTyAq&suffix=tar.gz" -O  GeoLite2-Country.tar.gz
+wget -q "https://download.maxmind.com/app/geoip_download?edition_id=GeoLite2-Country&license_key=$license_key&suffix=tar.gz" -O  GeoLite2-Country.tar.gz
 
 tar zxf GeoLite2-Country.tar.gz
 
